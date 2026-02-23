980082571bddd12846644bc31a378cf36757c794
LucasFigueroa <lucasafigueroa93@gmail.com>
2026-02-23 19:33:54 -0300

    Fix: Conexion a mongo

--- src/config/connections/mongo.js
+++ src/config/connections/mongo.js
@@ -1,20 +1,24 @@
-import mongoose from 'mongoose';
-import { log, error } from '../../utils/logger.js';
-import 'dotenv/config';
+import mongoose from "mongoose";
+import { log, error } from "../../utils/logger.js";
+import "dotenv/config";
+
+let isConnected = false;
 
 export const connectToMongo = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_KEY, {
-            dbName: 'DB-Glamour',
-            serverSelectionTimeoutMS: 10000,
-            socketTimeoutMS: 45000,
-            maxPoolSize: 10,
-            retryWrites: true,
-            w: 'majority'
-        });
-        log('Conexión a la base de datos exitosa');
-    } catch (err) {
-        error('Error al conectarse a la base de datos', err.message);
-        process.exit(1);
-    }
+  if (isConnected) return;
+  try {
+    const db = await mongoose.connect(process.env.MONGO_KEY, {
+      dbName: "DB-Glamour",
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
+      maxPoolSize: 10,
+      retryWrites: true,
+      w: "majority",
+    });
+    isConnected = db.connections[0].readyState === 1;
+    log("Conexión a la base de datos exitosa");
+  } catch (err) {
+    error("Error al conectarse a la base de datos", err.message);
+    throw err;
+  }
 };
